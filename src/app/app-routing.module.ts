1a7e605566d38562f490cb90dc050cbec1a97d65
erwin-io <erwinramirez220@gmail.com>
2022-08-13 03:28:12 +0800

    1st commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,32 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { AuthGuard } from './core/guard/auth.guard';
+import { FeaturesComponent } from './pages/features/features.component';
+
+
+const routes: Routes = [
+    { path: '', pathMatch: 'full', redirectTo: 'dashboard' },
+    { path: '',
+      component: FeaturesComponent,
+      canActivate: [AuthGuard],
+      children: [
+        { path: 'dashboard', canActivate: [AuthGuard], loadChildren: () => import('./pages/features/home/home.module').then(m => m.HomeModule) },
+        { path: 'security', canActivate: [AuthGuard], loadChildren: () => import('./pages/features/security/security.module').then(m => m.SecurityModule) },
+      ]
+    },
+    { path: 'account',
+      loadChildren:  () => import('./pages/accounts/accounts.module').then( m => m.AccountsModule) ,
+      canActivate: [AuthGuard]
+    },
+    { path: 'auth',
+      loadChildren: () => import('./pages/auth/auth.module').then( m => m.AuthModule)
+    },
+
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,42 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TokenInterceptor } from './core/interceptors/token.interceptors';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { MaterialModule } from './material/material.module';
+import { CommonModule } from '@angular/common';
+import { FeaturesComponent } from './pages/features/features.component';
+import { MenuListItemComponent } from './pages/features/ui/menu-list-item/menu-list-item.component';
+import { FormFieldErrorComponent } from './shared/form-field-error/form-field-error.component';
+import { MAT_SNACK_BAR_DEFAULT_OPTIONS } from '@angular/material/snack-bar';
+import { NgxSkeletonLoaderModule } from 'ngx-skeleton-loader';
+import { SnackbarComponent } from './shared/snackbar/snackbar.component';
+import { AlertDialogComponent } from './shared/alert-dialog/alert-dialog.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuListItemComponent,
+    FeaturesComponent,
+    FormFieldErrorComponent,
+    SnackbarComponent,
+    AlertDialogComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    CommonModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MaterialModule,
+    NgxSkeletonLoaderModule,
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi: true },
+    { provide: MAT_SNACK_BAR_DEFAULT_OPTIONS, useValue: {duration: 2500} }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
